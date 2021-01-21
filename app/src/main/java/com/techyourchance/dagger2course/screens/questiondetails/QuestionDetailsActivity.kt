b39f5d9936a2d7430da37ec41dbafb2d21b79048
Hyukjoong Kim <hyukj.kim@samsung.com>
2021-01-21 12:47:47 +0900

    Extraction of UI Logic on QuestionsDetail screen
    Signed-off-by: Hyukjoong Kim <hyukj.kim@samsung.com>

--- app/src/main/java/com/techyourchance/dagger2course/screens/questiondetails/QuestionDetailsActivity.kt
+++ app/src/main/java/com/techyourchance/dagger2course/screens/questiondetails/QuestionDetailsActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.text.Html
+import android.view.LayoutInflater
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
@@ -17,31 +18,21 @@ import kotlinx.coroutines.*
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class QuestionDetailsActivity : AppCompatActivity() {
+class QuestionDetailsActivity : AppCompatActivity(), QuestionDetailsViewMvc.Listener {
 
     private val coroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
-
-    private lateinit var toolbar: MyToolbar
-    private lateinit var swipeRefresh: SwipeRefreshLayout
-    private lateinit var txtQuestionBody: TextView
 
     private lateinit var stackoverflowApi: StackoverflowApi
 
     private lateinit var questionId: String
 
+    private lateinit var detailsViewMvc: QuestionDetailsViewMvc
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.layout_question_details)
+        detailsViewMvc = QuestionDetailsViewMvc(LayoutInflater.from(this), null)
 
-        txtQuestionBody = findViewById(R.id.txt_question_body)
-
-        // init toolbar
-        toolbar = findViewById(R.id.toolbar)
-        toolbar.setNavigateUpListener { onBackPressed() }
-
-        // init pull-down-to-refresh (used as a progress indicator)
-        swipeRefresh = findViewById(R.id.swipeRefresh)
-        swipeRefresh.isEnabled = false
+        setContentView(detailsViewMvc.rootView)
 
         // init retrofit
         val retrofit = Retrofit.Builder()
@@ -56,27 +47,28 @@ class QuestionDetailsActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
+        detailsViewMvc.registerListener(this)
         fetchQuestionDetails()
     }
 
     override fun onStop() {
         super.onStop()
+        detailsViewMvc.unregisterListener(this)
         coroutineScope.coroutineContext.cancelChildren()
+    }
+
+    override fun onBackClicked() {
+        onBackPressed()
     }
 
     private fun fetchQuestionDetails() {
         coroutineScope.launch {
-            showProgressIndication()
+            detailsViewMvc.showProgressIndication()
             try {
                 val response = stackoverflowApi.questionDetails(questionId)
                 if (response.isSuccessful && response.body() != null) {
                     val questionBody = response.body()!!.question.body
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                        txtQuestionBody.text = Html.fromHtml(questionBody, Html.FROM_HTML_MODE_LEGACY)
-                    } else {
-                        @Suppress("DEPRECATION")
-                        txtQuestionBody.text = Html.fromHtml(questionBody)
-                    }
+                    detailsViewMvc.bindQuestion(questionBody)
                 } else {
                     onFetchFailed()
                 }
@@ -85,7 +77,7 @@ class QuestionDetailsActivity : AppCompatActivity() {
                     onFetchFailed()
                 }
             } finally {
-                hideProgressIndication()
+                detailsViewMvc.hideProgressIndication()
             }
 
         }
@@ -95,14 +87,6 @@ class QuestionDetailsActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
                 .add(ServerErrorDialogFragment.newInstance(), null)
                 .commitAllowingStateLoss()
-    }
-
-    private fun showProgressIndication() {
-        swipeRefresh.isRefreshing = true
-    }
-
-    private fun hideProgressIndication() {
-        swipeRefresh.isRefreshing = false
     }
 
     companion object {
---
+++ app/src/main/java/com/techyourchance/dagger2course/screens/questiondetails/QuestionDetailsViewMvc.kt
@@ -0,0 +1,75 @@
+package com.techyourchance.dagger2course.screens.questiondetails
+
+import android.os.Build
+import android.text.Html
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.annotation.IdRes
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.techyourchance.dagger2course.R
+import com.techyourchance.dagger2course.screens.common.toolbar.MyToolbar
+
+class QuestionDetailsViewMvc (
+        private val layoutInflater: LayoutInflater,
+        private val parent: ViewGroup?
+){
+
+    interface Listener {
+        fun onBackClicked()
+    }
+
+    val rootView: View = layoutInflater.inflate(R.layout.layout_question_details, parent, false)
+
+    private lateinit var toolbar: MyToolbar
+    private lateinit var swipeRefresh: SwipeRefreshLayout
+    private lateinit var txtQuestionBody: TextView
+
+    private val listeners = HashSet<Listener>()
+
+    init {
+        txtQuestionBody = findViewById(R.id.txt_question_body)
+
+        // init toolbar
+        toolbar = findViewById(R.id.toolbar)
+        toolbar.setNavigateUpListener {
+            for (listener in listeners) {
+                listener.onBackClicked()
+            }
+        }
+
+        // init pull-down-to-refresh (used as a progress indicator)
+        swipeRefresh = findViewById(R.id.swipeRefresh)
+        swipeRefresh.isEnabled = false
+    }
+
+    fun <T: View?> findViewById(@IdRes id: Int): T {
+        return rootView.findViewById<T>(id)
+    }
+
+    fun registerListener(listener: Listener) {
+        listeners.add(listener)
+    }
+
+    fun unregisterListener(listener: Listener) {
+        listeners.remove(listener)
+    }
+
+    fun showProgressIndication() {
+        swipeRefresh.isRefreshing = true
+    }
+
+    fun hideProgressIndication() {
+        swipeRefresh.isRefreshing = false
+    }
+
+    fun bindQuestion(questionBody: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            txtQuestionBody.text = Html.fromHtml(questionBody, Html.FROM_HTML_MODE_LEGACY)
+        } else {
+            @Suppress("DEPRECATION")
+            txtQuestionBody.text = Html.fromHtml(questionBody)
+        }
+    }
+}
